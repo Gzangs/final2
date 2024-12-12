79c39e5c2fd1d8841025fe5dcebbee2927b7efba
Gzangs <gzangs@unomaha.edu>
2024-12-12 17:44:05 -0600

    up

---
+++ functions2.py
@@ -0,0 +1,13 @@
+def calculate_final_grade(scores) -> float:
+    """
+    Calculates the final grade as the mean
+    float: mean grade
+    """
+    total_score = sum(scores)
+    num_assignments = len(scores)
+    mean_percentage = (total_score / (num_assignments * 100)) * 100
+    return round(mean_percentage, 2)
+
+
+
+
